f71fb141c0bc77674ace14119eb20807922998ba
Sarah Rosanna Busch <annarose0113@gmail.com>
2020-10-13 11:42:15 -0700

    updating to include portfolio stuff

--- main.js
+++ main.js
@@ -7,8 +7,34 @@
 
 var main = (function() {
     var that = {};
+    var elem = {};
+
+    var openPage = ''; //keep track of what's open in side panel
+
     that.init = function() {
-        
+        elem.sidePanel = f.html.getElem('#sidePanel');
+        elem.sideView = f.html.getElem('#sideView');
+        footer.setup();
     }
+
+    that.nav = function(url) {
+        if(window.innerWidth < 930) {
+            location.assign(url);
+        } else {
+            if(url === openPage) {
+                elem.sidePanel.style.display = 'none';
+                openPage = '';
+            } else {
+                main.embedPage(url);
+            }
+        }
+    }
+
+    that.embedPage = function(url) {
+        elem.sideView.src = url;
+        elem.sidePanel.style.display = 'inline-block';
+        openPage = url;
+    }
+
     return that;
 }());
